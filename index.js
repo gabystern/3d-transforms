b1dd38436ff6767fa8262585fc93672087bd96d1
gabystern <stern.gabrielle@gmail.com>
2017-08-07 16:33:43 -0400

    added alert when item added to cart

--- index.js
+++ index.js
@@ -5,3 +5,7 @@ function bedPrice() {
   let div = document.getElementById("pricetag");
   div.innerHTML = value;
 }
+
+function cartAlert(){
+  alert("You're one step closer to your dream sleep. Your bed has been added to your cart.")
+}
